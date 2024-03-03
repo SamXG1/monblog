6106ee808dad7fbdbaf1328f49c0fe0ec671ee87
SamXG1 <161257830+SamXG1@users.noreply.github.com>
2024-03-03 17:28:53 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Contenu/loginStyles.css">
+    <style>
+       
+    </style>
+    <title>Mon - Blog Se connecter</title>
+</head>
+<body>
+    
+</body>
+</html>
+<body>
+    <div class="wrapper">
+        <section class="form login">
+            <h1>Se connecter</h1>
+            <form action="#" class="login-form">
+                <div class="error-text"></div>
+                    <div class=" field input">
+                        <label>Adresse e-mail</label>
+                        <input type="text" name="email" placeholder="Entrer votre Email">
+                    </div>
+                    <div class=" field input">
+                        <label>Mot de passe</label>
+                        <input type="password" name="password" placeholder="Tapez votre mot de passe">
+                    </div>
+                    <div class=" field button">
+                        <input type="submit" value="continuer sur le blog">
+                    </div>
+            </form>
+        </section>
+    </div>
+    <script src="javascript/login.js"></script>
+</body>
+</html>
